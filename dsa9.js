56a8e8daeeda6b9bc6dd4968b8f7226a21e7518a
sakshimandloi <sakshimandloi4722@gmail.com>
2023-10-23 15:53:47 +0530

    this is my 9th commit

---
+++ dsa9.js
@@ -0,0 +1,11 @@
+const number = 5;
+if (number > 0)
+ {
+  console.log("the number is possitive");
+}
+else if (number == 0) {
+   console.log("the number is zero")
+}
+else {
+    ("number is negative")
+}
