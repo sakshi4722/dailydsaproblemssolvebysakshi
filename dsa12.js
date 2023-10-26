fa01e9a67d11b62cc4bfbbd001f7fd84bcfff02d
sakshimandloi <sakshimandloi4722@gmail.com>
2023-10-26 22:00:40 +0530

    this is my 12th commit

---
+++ dsa12.js
@@ -0,0 +1,9 @@
+// program to generate a multiplication table
+
+// take input from the user
+// 
+let n = 5;
+for (let i = 1; i <= 10; ++i)
+    console.log(n + " * " + i +
+        " = " + n * i);
+
